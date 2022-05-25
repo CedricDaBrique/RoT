d48a43ec0408ba7baf164c37b9d94ec81b30b636
CedricDaBrique <sysnographe@gmail.com>
2022-05-25 14:24:45 +0200

    ajout laser

--- src/Tableau1.js
+++ src/Tableau1.js
@@ -4,7 +4,7 @@ class Tableau1 extends Phaser.Scene {
     preload() {
 
         this.load.image("tilemap2", "assets/tiled.png");
-         //d
+        //d
         this.load.image("bg", "assets/TEST1.png");
         this.load.image("bgf", "assets/bg2.png");
         this.load.image("bgf2", "assets/bg3.png");
@@ -14,20 +14,20 @@ class Tableau1 extends Phaser.Scene {
         // chargement de la map en json
         this.load.tilemapTiledJSON("map", "assets/MapBasique.json");
         //chargement des animations
-        for(let i=1;i<=5;i++){
-            this.load.image('player'+i, 'assets/idle/idle'+i+'.png');
+        for (let i = 1; i <= 5; i++) {
+            this.load.image('player' + i, 'assets/idle/idle' + i + '.png');
         }
-        for(let i=1;i<=24;i++){
-            this.load.image('laser'+i, 'assets/animation/laser'+i+'.png');
+        for (let i = 1; i <= 24; i++) {
+            this.load.image('laser' + i, 'assets/animation/laser/laser' + i + '.png');
         }
-        for(let i=1;i<=4;i++){
-            this.load.image('dash'+i, 'assets/animation/dash/dash'+i+'.png');
+        for (let i = 1; i <= 4; i++) {
+            this.load.image('dash' + i, 'assets/animation/dash/dash' + i + '.png');
         }
-        for(let i=1;i<=7;i++){
-            this.load.image('jump'+i, 'assets/animation/jump/jump'+i+'.png');
+        for (let i = 1; i <= 7; i++) {
+            this.load.image('jump' + i, 'assets/animation/jump/jump' + i + '.png');
         }
-        for(let i=1;i<=6;i++){
-            this.load.image('move'+i, 'assets/animation/Move/Move'+i+'.png');
+        for (let i = 1; i <= 6; i++) {
+            this.load.image('move' + i, 'assets/animation/Move/Move' + i + '.png');
         }
     }
 
@@ -45,13 +45,13 @@ class Tableau1 extends Phaser.Scene {
 
 
         //Ajout de l'arrière plan
-        this.bg2 = this.add.sprite(0,0, 'bg').setOrigin(0,0);
-        this.bg = this.add.sprite(0,0, 'bg').setOrigin(0,1);
-        this.bg3 = this.add.sprite(0,0, 'bg').setOrigin(0,2);
-        this.bgf = this.add.sprite(0,0, 'bgf').setOrigin(0,3);
-        this.so = this.add.sprite(-3010,1900, 'so').setOrigin(0,0);
-        this.so2 = this.add.sprite(-2010,1900, 'so').setOrigin(0,0);
-        this.so3 = this.add.sprite(-1010,2000, 'so').setOrigin(0,0);
+        this.bg2 = this.add.sprite(0, 0, 'bg').setOrigin(0, 0);
+        this.bg = this.add.sprite(0, 0, 'bg').setOrigin(0, 1);
+        this.bg3 = this.add.sprite(0, 0, 'bg').setOrigin(0, 2);
+        this.bgf = this.add.sprite(0, 0, 'bgf').setOrigin(0, 3);
+        this.so = this.add.sprite(-3010, 1900, 'so').setOrigin(0, 0);
+        this.so2 = this.add.sprite(-2010, 1900, 'so').setOrigin(0, 0);
+        this.so3 = this.add.sprite(-1010, 2000, 'so').setOrigin(0, 0);
 
         this.speed = {
             speedDash: 1,
@@ -63,7 +63,7 @@ class Tableau1 extends Phaser.Scene {
             ease: "Circ.easeInOut", // 'Cubic', 'Elastic', 'Bounce', 'Back'
             duration: 300,
             //onUpdate : function (){
-                //this.player.setVelocityX(900 * this.speedDash);
+            //this.player.setVelocityX(900 * this.speedDash);
             //}
 
         });
@@ -102,12 +102,14 @@ class Tableau1 extends Phaser.Scene {
             "plateforme",
             tileset2
         );
-        const spike = map.createLayer(
-            "spike",
+
+        const decors = map.createLayer(
+            "decors",
             tileset2
         );
-        const decors= map.createLayer(
-            "decors",
+
+        const spike = map.createLayer(
+            "spike",
             tileset2
         );
 
@@ -116,7 +118,7 @@ class Tableau1 extends Phaser.Scene {
             immovable: true
         });
         map.getObjectLayer('platforms_colliders').objects.forEach((platform) => {
-            this.specialesboup = this.speciales.create(platform.x, platform.y-platform.height,'vide').setOrigin(0).setDisplaySize(platform.width,platform.height);
+            this.specialesboup = this.speciales.create(platform.x, platform.y - platform.height, 'vide').setOrigin(0).setDisplaySize(platform.width, platform.height);
         });
 
         this.collidersMur = this.physics.add.group({
@@ -125,17 +127,13 @@ class Tableau1 extends Phaser.Scene {
         });
         map.getObjectLayer('colliders').objects.forEach((collider) => {
 
-            this.colliderMurboup = this.collidersMur.create(collider.x, collider.y,'vide')
-                                                    .setRotation(Phaser.Math.DegToRad(collider.rotation))
-                                                    .setOrigin(0)
-                                                    .setDisplaySize(collider.width,collider.height);
+            this.colliderMurboup = this.collidersMur.create(collider.x, collider.y, 'vide')
+                .setRotation(Phaser.Math.DegToRad(collider.rotation))
+                .setOrigin(0)
+                .setDisplaySize(collider.width, collider.height);
         });
 
         platforms.setCollisionByExclusion(-1, true);
-
-
-
-
 
 
         // Création du personnage de base
@@ -153,11 +151,11 @@ class Tableau1 extends Phaser.Scene {
             key: 'player',
             //frames: this.getFrames('player', 5),
             frames: [
-                {key:'player1'},
-                {key:'player2'},
-                {key:'player3'},
-                {key:'player4'},
-                {key:'player5'},
+                {key: 'player1'},
+                {key: 'player2'},
+                {key: 'player3'},
+                {key: 'player4'},
+                {key: 'player5'},
             ],
             frameRate: 5,
             repeat: -1,
@@ -167,13 +165,13 @@ class Tableau1 extends Phaser.Scene {
         this.anims.create({
             key: 'jump',
             frames: [
-                {key:'jump1'},
-                {key:'jump2'},
-                {key:'jump3'},
-                {key:'jump4'},
-                {key:'jump5'},
-                {key:'jump6'},
-                {key:'jump7'},
+                {key: 'jump1'},
+                {key: 'jump2'},
+                {key: 'jump3'},
+                {key: 'jump4'},
+                {key: 'jump5'},
+                {key: 'jump6'},
+                {key: 'jump7'},
             ],
             frameRate: 7,
 
@@ -182,10 +180,10 @@ class Tableau1 extends Phaser.Scene {
         this.anims.create({
             key: 'dash',
             frames: [
-                {key:'dash1'},
-                {key:'dash2'},
-                {key:'dash3'},
-                {key:'dash4'},
+                {key: 'dash1'},
+                {key: 'dash2'},
+                {key: 'dash3'},
+                {key: 'dash4'},
             ],
             frameRate: 4,
         });
@@ -193,48 +191,48 @@ class Tableau1 extends Phaser.Scene {
         this.anims.create({
             key: 'move',
             frames: [
-                {key:'move1'},
-                {key:'move2'},
-                {key:'move3'},
-                {key:'move4'},
-                {key:'move5'},
-                {key:'move6'},
+                {key: 'move1'},
+                {key: 'move2'},
+                {key: 'move3'},
+                {key: 'move4'},
+                {key: 'move5'},
+                {key: 'move6'},
             ],
             frameRate: 6,
             repeat: -1,
         });
 
-        // this.anims.create({
-        //     key: 'laser',
-        //     frames: [
-        //         {key:'laser1'},
-        //         {key:'laser2'},
-        //         {key:'laser3'},
-        //         {key:'laser4'},
-        //         {key:'laser5'},
-        //         {key:'laser6'},
-        //         {key:'laser7'},
-        //         {key:'laser8'},
-        //         {key:'laser9'},
-        //         {key:'laser10'},
-        //         {key:'laser11'},
-        //         {key:'laser12'},
-        //         {key:'laser13'},
-        //         {key:'laser14'},
-        //         {key:'laser15'},
-        //         {key:'laser16'},
-        //         {key:'laser17'},
-        //         {key:'laser18'},
-        //         {key:'laser19'},
-        //         {key:'laser20'},
-        //         {key:'laser21'},
-        //         {key:'laser22'},
-        //         {key:'laser23'},
-        //         {key:'laser24'},
-        //     ],
-        //     frameRate: 24,
-        //     repeat: -1,
-        // });
+        this.anims.create({
+            key: 'laser',
+            frames: [
+                {key: 'laser1'},
+                {key: 'laser2'},
+                {key: 'laser3'},
+                {key: 'laser4'},
+                {key: 'laser5'},
+                {key: 'laser6'},
+                {key: 'laser7'},
+                {key: 'laser8'},
+                {key: 'laser9'},
+                {key: 'laser10'},
+                {key: 'laser11'},
+                {key: 'laser12'},
+                {key: 'laser13'},
+                {key: 'laser14'},
+                {key: 'laser15'},
+                {key: 'laser16'},
+                {key: 'laser17'},
+                {key: 'laser18'},
+                {key: 'laser19'},
+                {key: 'laser20'},
+                {key: 'laser21'},
+                {key: 'laser22'},
+                {key: 'laser23'},
+                {key: 'laser24'},
+            ],
+            frameRate: 24,
+
+        });
 
 
         // Creation des collision
@@ -253,44 +251,81 @@ class Tableau1 extends Phaser.Scene {
         this.initKeyboard();
 
         const objectsLayer = map.getObjectLayer('tp')
-        objectsLayer.objects.forEach(objData=> {
+        objectsLayer.objects.forEach(objData => {
             const {x = 0, y = 0, name} = objData
 
             switch (name) {
                 case 'tp1': {
-                    this.tp1 = this.physics.add.sprite(x, y-200, 'player').setOrigin(0, 0);
+                    this.tp1 = this.physics.add.sprite(x, y - 200, 'player').setOrigin(0, 0);
                     this.tp1.setDisplaySize(64, 64);
                     this.tp1.body.setAllowGravity(true);
                     this.tp1.setVisible(true);
 
 
-                    this.physics.add.collider(this.tp1,this.collidersMur)
+                    this.physics.add.collider(this.tp1, this.collidersMur)
 
                 }
                 case 'tp2': {
 
                     let me = this;
-                    this.tp2 = this.physics.add.sprite(x, y-200, 'player').setOrigin(0, 0);
+                    this.tp2 = this.physics.add.sprite(x, y - 200, 'player').setOrigin(0, 0);
                     this.tp2.setDisplaySize(64, 64);
                     this.tp2.body.setAllowGravity(true);
                     this.tp2.setVisible(true);
 
-                    this.physics.add.collider(this.tp2,this.collidersMur)
+                    this.physics.add.collider(this.tp2, this.collidersMur)
 
                     this.physics.add.overlap(this.player, this.tp1, function () {
 
                         me.player.x = me.tp2.x
-                        me.player.y = me.tp2.y-100
+                        me.player.y = me.tp2.y - 100
                     })
-
 
 
                 }
             }
         })
 
+        const objectsLayerLaser = map.getObjectLayer('laser')
+        objectsLayerLaser.objects.forEach(objData => {
+            const {x = 0, y = 0, name} = objData
+            {
+                const {x = 0, y = 0, name} = objData
 
+                switch (name) {
+                    case 'laser1': {
+
+                        this.laser1 = this.physics.add.sprite(x, y - 200, 'laser1').setOrigin(0, 0);
+                        this.laser1.body.setAllowGravity(false);
+                        this.laser1.setVisible(true);
+
+
+
+                        this.time.addEvent({
+                            delay: 5000,
+                            callback: ()=>{
+                                this.laser1.play("laser")
+                            },
+                            loop: true
+                        })
+
+
+
+
+                        this.laser1.x = x;
+                        this.laser1.y = y;
+
+
+
+
+                    }
+                }
+            }
+        })
     }
+
+
+
 
     initKeyboard() {
         let me = this;
@@ -374,6 +409,8 @@ class Tableau1 extends Phaser.Scene {
 
     update(){
 
+        // this.player.x = this.laser1.x
+        // this.player.y = this.laser1.y - 100
 
         console.log(this.tp1.x)
         console.log(this.player.x)
